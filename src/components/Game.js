a1e4f38e93a076cfac2134f9eb0cbda1fcbb8fed
Heather Stock <heatherstock@Jamess-MacBook-Pro-2.local>
2018-06-17 15:02:05 +0100

    :hankey: Implemented redundant method to practice updating state with button click

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,16 +7,22 @@ class Game extends Component {
     super();
     this.state = {
       gameScore1: gamePoints[0],
-      gameScore2: gamePoints[0]
+      gameScore2: gamePoints[0],
+      test: 0
     };
   }
 
+  click = () => {
+    this.setState({ test: this.state.test + 1 });
+  }
+
   render() {
+    console.log(this.state.test)
     return (
       <div>
         <h2>Game</h2>
-        <Player name={1} gameScore={this.state.gameScore1}/>
-        <Player name={2} gameScore={this.state.gameScore2}/>
+        <Player name={1} gameScore={this.state.gameScore1} click={this.click}/>
+        <Player name={2} gameScore={this.state.gameScore2} click={this.click}/>
       </div>
     );
   };
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Player = ({ name, gameScore }) => {
+const Player = ({ name, gameScore, click }) => {
   return (
     <div>
       <h3>Player {name}: {gameScore}</h3>
+      <button id={name} onClick={click}>Score</button>
     </div>
   )
 }
