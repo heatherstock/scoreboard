3b9c6ed3ce04ef06001087f05721ff89943a83f7
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-06-25 12:02:43 +0100

    :white_check_mark: Completed logic and tests for all score possibilities

--- src/logic/logic.js
+++ src/logic/logic.js
@@ -5,39 +5,65 @@ export const computeScore = (player, score) => {
   const opposition = (player === 1) ? 2 : 1;
   
   //get current points from state passed down as argument
-  const playerCurrentPoints = score.game['player' + player]
-  const oppositionCurrentPoints = score.game['player' + opposition]
+  const playerCurrentPoints = score.game['player' + player];
+  const oppositionCurrentPoints = score.game['player' + opposition];
+
+  const playerSet = score.set['player' + player];
+  const oppositionSet = score.set['player' + opposition];
+
+  const playerMatch = score.match['player' + player];
+  const oppositionMatch = score.match['player' + opposition];
   
   //get index of current points from gamePoints array (above) in order to do simple calculations (below)
   const playerIndex = gamePoints.indexOf(playerCurrentPoints);
-  const oppositionIndex = gamePoints.indexOf(oppositionCurrentPoints)
+  const oppositionIndex = gamePoints.indexOf(oppositionCurrentPoints);
 
   //define key points in game
-  const winningScore = ((playerIndex === 4 && oppositionIndex === 3) || (playerIndex === 3 && oppositionIndex < 3))
-  const resetScore = (playerCurrentPoints === 'WINNER' || playerCurrentPoints === 'LOSER')
-  const loseAdvantage = (oppositionIndex === 4 && playerIndex === 3)
-  const normalScore = ((playerIndex < 3 && oppositionIndex <= 3) || (playerIndex === 3 && oppositionIndex === 3))
+  const gamePoint = ((playerIndex === 4 && oppositionIndex === 3) || (playerIndex === 3 && oppositionIndex < 3));
+  const gameWon = (playerCurrentPoints === 'WINNER' || playerCurrentPoints === 'LOSER');
+  const scoreAgainstAdvantage = (oppositionIndex === 4 && playerIndex === 3);
+  const normal = ((playerIndex < 3 && oppositionIndex <= 3) || (playerIndex === 3 && oppositionIndex === 3));
+  const setPoint = (playerSet >= 5) && (playerSet >= oppositionSet + 1);
 
   let result;
 
-  if (resetScore) {
-    result = { ['player' + player]: gamePoints[1], 
-        ['player' + opposition]: gamePoints[0] };
+  if (setPoint && gamePoint) {
+    result = { 
+      game: { ['player' + player]: 'WINNER', 
+      ['player' + opposition]: 'LOSER' },
+
+      set: { ['player' + player]: 0,
+      ['player' + opposition]: 0  },
+
+      match: { ['player' + player]: playerMatch + 1, 
+      ['player' + opposition]: oppositionMatch }
+    };
+    return result;
+  }
+
+  if (gamePoint && !setPoint) {
+    result = { game: { ['player' + player]: 'WINNER', 
+        ['player' + opposition]: 'LOSER' },
+      set: { ['player' + player]: playerSet + 1, 
+        ['player' + opposition]: oppositionSet }};
+    return result;
+  }
+
+  if (gameWon) {
+    result = { game: { ['player' + player]: gamePoints[1], 
+        ['player' + opposition]: gamePoints[0] }};
     return result; 
   }
-  if (winningScore) {
-    result = { ['player' + player]: 'WINNER', 
-        ['player' + opposition]: 'LOSER' };
+
+  if (scoreAgainstAdvantage) {
+    result = { game: { ['player' + player]: playerCurrentPoints, 
+        ['player' + opposition]: gamePoints[oppositionIndex - 1] }};
     return result;
   }
-  if (loseAdvantage) {
-    result = { ['player' + player]: playerCurrentPoints, 
-        ['player' + opposition]: gamePoints[oppositionIndex - 1] }
-    return result;
-  }
-  if (normalScore) {
-    result = { ['player' + player]: gamePoints[playerIndex + 1], 
-    ['player' + opposition]: oppositionCurrentPoints };
+
+  if (normal) {
+    result = { game: { ['player' + player]: gamePoints[playerIndex + 1], 
+    ['player' + opposition]: oppositionCurrentPoints }};
     return result;
   }
 }
--- test/logic.spec.js
+++ test/logic.spec.js
@@ -4,51 +4,239 @@ import {computeScore} from '../src/logic/logic';
 import 'babel-polyfill';
 
 describe('computeScore', () => {
+  context('when a player scores a "normal" point (i.e. not deuce, advantage or at game point or set point)', () => {
     it('increases the score by the correct number of points', () => {
-        let player = 1;
-        let gameScore = {player1: 15, player2: 15};
+      let player = 1;
+      let score = {
+        game: {
+          player1: 15,
+          player2: 15
+        },
+        set: {
+          player1: 0,
+          player2: 0
+        },
+        match: {
+          player1: 0,
+          player2: 0
+        }
+      };
 
-        expect(computeScore(player, gameScore))
-            .to.deep.equal({ player1: 30, player2: 15 })
-    })
+      expect(computeScore(player, score))
+          .to.deep.equal({ game: { player1: 30, player2: 15 }});
+    });
+  });
 
-    it('announces the winner after 40 points if not on deuce', () => {
-        let player = 1;
-        let gameScore = {player1: 40, player2: 15};
+  context('when a player scores a point at game point, but not set point', () => {
+    it('announces the winner after 40 points if not on deuce, and updates set', () => {
+      let player = 1;
+      let score = {
+        game: {
+          player1: 40,
+          player2: 15
+        },
+        set: {
+          player1: 0,
+          player2: 0
+        },
+        match: {
+          player1: 0,
+          player2: 0
+        }
+      };
 
-        expect(computeScore(player, gameScore))
-            .to.deep.equal({ player1: 'WINNER', player2: 'LOSER' })
-    })
+      expect(computeScore(player, score))
+        .to.deep.equal({ 
+            game: { player1: 'WINNER', player2: 'LOSER' }, 
+            set: { player1: 1, player2: 0 }
+        });
+    });
 
     it('increases score to advantage if on deuce', () => {
-        let player = 1;
-        let gameScore = {player1: 40, player2: 40};
+      let player = 1;
+      let score = {
+        game: {
+          player1: 40,
+          player2: 40
+        },
+        set: {
+          player1: 0,
+          player2: 0
+        },
+        match: {
+          player1: 0,
+          player2: 0
+        }
+      };
 
-        expect(computeScore(player, gameScore))
-            .to.deep.equal({ player1: 'ADVANTAGE', player2: 40 })
-    })
+      expect(computeScore(player, score))
+        .to.deep.equal({ game: { player1: 'ADVANTAGE', player2: 40 }});
+    });
 
-    it('announces the winner after advantage', () => {
-        let player = 1;
-        let gameScore = {player1: 'ADVANTAGE', player2: 40};
+    it('announces the winner after winning advantage and updates set', () => {
+      let player = 1;
+      let score = {
+        game: {
+          player1: 'ADVANTAGE',
+          player2: 40
+        },
+        set: {
+          player1: 0,
+          player2: 0
+        },
+        match: {
+          player1: 0,
+          player2: 0
+        }
+      };
 
-        expect(computeScore(player, gameScore))
-            .to.deep.equal({ player1: 'WINNER', player2: 'LOSER' })
-    })
+      expect(computeScore(player, score))
+        .to.deep.equal({ 
+          game: { player1: 'WINNER', player2: 'LOSER' }, 
+          set: { player1: 1, player2: 0 }
+        });
+    });
 
     it('decreases the oppositions score if they had advantage', () => {
-        let player = 1;
-        let gameScore = {player1: 40, player2: 'ADVANTAGE'};
+      let player = 1;
+      let score = {
+        game: {
+          player1: 40,
+          player2: 'ADVANTAGE'
+        },
+        set: {
+          player1: 0,
+          player2: 0
+        },
+        match: {
+          player1: 0,
+          player2: 0
+        }
+      };
 
-        expect(computeScore(player, gameScore))
-            .to.deep.equal({ player1: 40, player2: 40 })
-    })
+      expect(computeScore(player, score))
+        .to.deep.equal({ game: { player1: 40, player2: 40 }});
+    });
 
-    it('resets the score after the winner is announced (and awards the correct number of points)', () => {
-        let player = 1;
-        let gameScore = {player1: 'WINNER', player2: 'LOSER'};
+    it('resets the current game after the winner is announced (and awards the correct number of points)', () => {
+      let player = 1;
+      let score = {
+        game: {
+          player1: 'WINNER',
+          player2: 'LOSER'
+        },
+        set: {
+          player1: 0,
+          player2: 0
+        },
+        match: {
+          player1: 0,
+          player2: 0
+        }
+      };
 
-        expect(computeScore(player, gameScore))
-            .to.deep.equal({ player1: 15, player2: 0 })
-    })
-})
+      expect(computeScore(player, score))
+        .to.deep.equal({ game: { player1: 15, player2: 0 }});
+    });
+  });
+  
+  context('when approaching set point', () => {
+    it('updates sets won and resets scores if first to reach 6 points with 2 points above opposition', () => {
+      let player = 1;
+      let score = {
+        game: {
+          player1: 40,
+          player2: 15
+        },
+        set: {
+          player1: 5,
+          player2: 0
+        },
+        match: {
+          player1: 0,
+          player2: 0
+        }
+      };
+
+      expect(computeScore(player, score))
+        .to.deep.equal({ 
+          game: { player1: 'WINNER', player2: 'LOSER' },
+          set: { player1: 0, player2: 0 },
+          match: { player1: 1, player2: 0 }
+        });
+    });
+
+    it('updates sets won and resets scores if above 6 points with 2 points above opposition', () => {
+      let player = 1;
+      let score = {
+        game: {
+          player1: 40,
+          player2: 15
+        },
+        set: {
+          player1: 7,
+          player2: 6
+        },
+        match: {
+          player1: 0,
+          player2: 0
+        }
+      };
+
+      expect(computeScore(player, score))
+        .to.deep.equal({ 
+          game: { player1: 'WINNER', player2: 'LOSER' },
+          set: { player1: 0, player2: 0 },
+          match: { player1: 1, player2: 0 }
+        });
+    });
+
+    it('increases games won score if less than 6 points', () => {
+      let player = 1;
+      let score = {
+        game: {
+          player1: 40,
+          player2: 15
+        },
+        set: {
+          player1: 3,
+          player2: 0
+        },
+        match: {
+          player1: 0,
+          player2: 0
+        }
+      };
+
+      expect(computeScore(player, score))
+        .to.deep.equal({ 
+          game: { player1: 'WINNER', player2: 'LOSER' },
+          set: { player1: 4, player2: 0 }
+        });
+    });
+
+    it('increases games won score if 6 points or above, but not 2 points above opposition', () => {
+      let player = 1;
+      let score = {
+        game: {
+          player1: 40,
+          player2: 15
+        },
+        set: {
+          player1: 6,
+          player2: 6
+        },
+        match: {
+          player1: 0,
+          player2: 0
+        }
+      };
+
+      expect(computeScore(player, score))
+        .to.deep.equal({ 
+          game: { player1: 'WINNER', player2: 'LOSER' },
+          set: { player1: 7, player2: 6 }
+        });
+    });
+  });
+});
